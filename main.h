c93c4f896f356b38ff7c05c19ab52b802b7a3ae2
eyoul <eyoulshimeles2@gmail.com>
2022-07-12 14:49:13 -0700

    repo

--- main.h
+++ main.h
@@ -12,8 +12,7 @@ typedef struct print
 	char *t;
 	int (*f)(va_list);
 } print_t;
-
-int get_flags(const char *format, int *i);
+int get_flag(char s, flags_t *f);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list c);
